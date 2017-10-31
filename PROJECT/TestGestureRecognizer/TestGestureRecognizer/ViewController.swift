dc5df70ce75e799ead122292b0cf03f3e4a87320
ssy828 <828yss@gmail.com>
2017-10-31 11:16:46 +0900

    update1030(2)

--- PROJECT/TestGestureRecognizer/TestGestureRecognizer/ViewController.swift
+++ PROJECT/TestGestureRecognizer/TestGestureRecognizer/ViewController.swift
@@ -8,18 +8,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIGestureRecognizerDelegate {
 
+    @IBOutlet weak var testView: UIView!
+    
+    @IBOutlet var testGesture: UITapGestureRecognizer!
+    
+    
+    @IBOutlet weak var countLb: UILabel!
+    
+    
+    @IBOutlet weak var locationLb: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldReceive touch: UITouch) -> Bool {
+        print("xpositon", touch.location(in: touch.view).x)
+        
+        print("touch time stamp", touch.timestamp)
+        
+        print("touch tapCount",touch.tapCount)
+        var touchCount = 0
+        let temp = touch.tapCount
+        touchCount += temp
+     
+        self.locationLb.text = "\(touch.location(in: touch.view).x)"
+        self.countLb.text = "\(touchCount)"
+        
+        return true
     }
-
-
+    
+    
 }
 
