597e344ea14aa19117173d223fbced3a5b3a62d2
ssy828 <828yss@gmail.com>
2017-11-17 00:30:07 +0900

    add signupViewController

---
+++ PROJECT/LoginAndJSON/LoginAndJSON/New Group/SignUpViewController.swift
@@ -0,0 +1,70 @@
+//
+//  SignUpViewController.swift
+//  LoginAndJSON
+//
+
+
+import UIKit
+
+//  회원 가입창실행
+
+class SignUpViewController: UIViewController {
+
+    var network = NetworkManager.one
+    
+    @IBOutlet weak var idTF: UITextField!
+    @IBOutlet weak var pwdTF: UITextField!
+    @IBOutlet weak var rePwdTF: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func signUp(_ sender: Any)
+    {
+        guard let userId = idTF.text, !userId.isEmpty else { return }
+        // isEmpty가 거짓이면 다음 가드문 실행
+        guard let userPwd = pwdTF.text, !userPwd.isEmpty else { return }
+        guard let userRePwd = rePwdTF.text, !userRePwd.isEmpty else { return }
+        
+        
+        // if문을 사용해 isEmpty를 쓰는게 나은 건지
+//        if userId.isEmpty || userPwd.isEmpty || userRePwd.isEmpty
+//        {
+//            //
+//        }
+        network.fetchSignup(userId: userId, userPwd: userPwd) { [weak self] isSuccess in
+             // 클로저를 통해서 성공했을 경우
+            guard let `self` = self else { return }
+            if isSuccess
+            {
+                let alertController = Alert.showAlert(title: "회원가입 성공!", msg: "회원가입을 축하합니다!", cancelAction: true, completion: { _ in
+                  
+//                    if let nextVC = self.storyboard?.instantiateViewController(withIdentifier: "Login") { self.navigationController?.pushViewController(nextVC, animated: true)}
+                self.dismiss(animated: true, completion: nil)
+                })
+                DispatchQueue.main.async {
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }else{
+                
+                if userPwd != userRePwd
+                {
+                    let alertController = Alert.showAlert(title: "실패", msg: "비밀번호가 다릅니다", cancelAction: true, completion: nil)
+                    DispatchQueue.main.async {
+                        self.present(alertController, animated: true, completion: nil)
+                    }
+                }
+            }
+        }
+        
+        
+        
+        
+    }
+    
+    
+
+}
